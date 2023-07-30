d60f95f71cdeaa77607c75548e7b5d977920b059
HansolLim <dev.hansollim@gmail.com>
2023-07-31 00:59:36 +0900

    union

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import os
 from glob import glob
 from pathlib import Path
-from typing import Generator, Optional
+from typing import Generator, Union
 
 
-def get_app_names(dir_path: Optional[str, Path]) -> Generator[str, None, None]:
+def get_app_names(dir_path: Union[str, Path]) -> Generator[str, None, None]:
     for _p in glob(os.path.join(dir_path, "*", "apps.py")):
         yield _p.split("/")[-2]
