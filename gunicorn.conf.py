168fdaa1485a44ac1bf17511b068ef55061bb95b
HansolLim <dev.hansollim@gmail.com>
2023-07-31 13:12:18 +0900

    without https

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 bind = "0.0.0.0:8000"
 workers = 1
 name = "dawith"
-
-certfile = "/etc/letsencrypt/live/dawith.us/fullchain.pem"
-keyfile = "/etc/letsencrypt/live/dawith.us/privkey.pem"
+#
+# certfile = "/etc/letsencrypt/live/dawith.us/fullchain.pem"
+# keyfile = "/etc/letsencrypt/live/dawith.us/privkey.pem"
