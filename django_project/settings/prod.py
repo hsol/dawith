3ab3f8b96b2ad674037962fc3643a4ebaab7b887
HansolLim <dev.hansollim@gmail.com>
2023-07-31 13:18:54 +0900

    https redirect

--- django_project/settings/prod.py
+++ django_project/settings/prod.py
@@ -1,5 +1,9 @@
 from .base import *
 
-CSRF_TRUSTED_ORIGINS = ["http://dawith.us"]
+CSRF_TRUSTED_ORIGINS = ["http://dawith.us", "https://dawith.us"]
 DEBUG = False
 MIDDLEWARE.append("django.middleware.cache.FetchFromCacheMiddleware")
+
+SECURE_SSL_REDIRECT = True
+SECURE_SSL_HOST = "dawith.us"
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,3 @@
 bind = "0.0.0.0:8000"
 workers = 1
 name = "dawith"
-#
-# certfile = "/etc/letsencrypt/live/dawith.us/fullchain.pem"
-# keyfile = "/etc/letsencrypt/live/dawith.us/privkey.pem"
