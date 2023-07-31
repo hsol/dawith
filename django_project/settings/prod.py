05ff8cf5dd04e1791888e0f190702a9423db1d5b
HansolLim <dev.hansollim@gmail.com>
2023-07-31 13:25:30 +0900

    일단 제거

--- django_project/settings/prod.py
+++ django_project/settings/prod.py
@@ -3,7 +3,3 @@ from .base import *
 CSRF_TRUSTED_ORIGINS = ["http://dawith.us", "https://dawith.us"]
 DEBUG = False
 MIDDLEWARE.append("django.middleware.cache.FetchFromCacheMiddleware")
-
-SECURE_SSL_REDIRECT = True
-SECURE_SSL_HOST = "dawith.us"
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
